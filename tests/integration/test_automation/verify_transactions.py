ef57a37758cc2a49259fb866199ea90bf3ab02e8
pcdeni <pcdeni@gmail.com>
2023-01-12 20:30:30 +0100

    parser start

--- tests/integration/test_automation/verify_transactions.py
+++ tests/integration/test_automation/verify_transactions.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python
 
 import subprocess
-
+import re
+    
 #if __name__ == "__main__":
-    # grep -rnwe 'data'
+# look for the address in main.map
+# grep -rnwe 'data'
 look_for_data_address = subprocess.check_output(['grep', '-rnwe', ' data'])
 look_for_data_address = look_for_data_address.splitlines()
 
@@ -19,7 +21,18 @@ for line in look_for_data_address:
         break
 print(address)
 
+# look for the mention of the address in the logs
 look_for_logs = subprocess.check_output(['grep', '-rnwe', address, '--exclude=trace_hart_*', '--exclude=main.map'])
 look_for_logs = look_for_logs.splitlines()
+log_entry = []
 for line in look_for_logs:
-    print(line)
+    L = re.split(' |> |:', line)
+    L = list(filter(None, L))
+    log_entry.append(L)
+    
+# sort the logs by simulation time
+log_entry.sort(key = lambda x: int(x[2]))
+for entry in log_entry:
+    print(entry)
+    
+
