25348752a3c2922f2c48019694dc4ae5ac862a62
Rocco Marasea <mail4rocco@gmail.com>
2023-04-19 13:18:05 +0200

    fix writeback_evict to work only within cache

--- tests/integration/testlist/writeback_evict/writeback_evict.c
+++ tests/integration/testlist/writeback_evict/writeback_evict.c
@@ -7,9 +7,11 @@ extern void exit(int);
 // cachelines are 128bit long
 #define uint128_t __uint128_t
 #define NUM_CACHELINES 256*6
+#define NUM_CACHELINES1 256*2
 //#define NUM_CORES 4
+
 uint128_t data[NB_CORES*NUM_CACHELINES] __attribute__((section(".cache_share_region")));
-uint128_t data1[NB_CORES*NUM_CACHELINES] __attribute__((section(".cache_share_region")));
+uint128_t data1[NB_CORES*NUM_CACHELINES1] __attribute__((section(".cache_share_region")));
 
 int writeback_evict(int cid, int nc)
 {
@@ -28,10 +30,10 @@ int writeback_evict(int cid, int nc)
   }
 
   // fill the cache with new cachelines
-  for (int i = 0; i < NUM_CACHELINES; i++) {
-    data1[cid*NUM_CACHELINES+i] = ~(cid*NUM_CACHELINES+i+1);
-    if (data1[cid*NUM_CACHELINES+i] != ~(cid*NUM_CACHELINES+i+1))
-      exit(~(cid*NUM_CACHELINES+i+1));
+  for (int i = 0; i < NUM_CACHELINES1; i++) {
+    data1[cid*NUM_CACHELINES1+i] = ~(cid*NUM_CACHELINES1+i+1);
+    if (data1[cid*NUM_CACHELINES1+i] != ~(cid*NUM_CACHELINES1+i+1))
+      exit(~(cid*NUM_CACHELINES1+i+1));
   }
 
   return 0;
